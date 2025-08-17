7369e2196d4c3cdd902d7ef2d048b2cba7e20e8f
yanlj666 <yanlj666@gmail.com>
2025-08-17 22:03:01 +0800

    feat: add chinese calligraphy fonts

--- HanInventor/src/main.js
+++ HanInventor/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import './assets/global.css'
 import router from './router'
 
 const app = createApp(App)
